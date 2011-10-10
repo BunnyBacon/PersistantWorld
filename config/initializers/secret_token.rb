4190a875ea4bd09c16b6775c1ccdc0188c559805
Robert Okon <okonpersonal@gmail.com>
2011-10-09 22:42:46 -0500

    commiting the initial project. Starting point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersistantWorld::Application.config.secret_token = '1888a355e72fa20a9d2673d939f20277be7ce5eedac4cd3bea6349ed4f9024dfab7a16c88c9dcd662a55ee1ec773605a84a7f026e069b3a5fbb652a253c1ec90'
